5b12e157d83eafbd13447f6842e002ebafb62f26
Mukund Rakholiya <155869925+mukund-rakholiya@users.noreply.github.com>
2024-12-29 05:42:00 +0530

    Update QuestionController.java

--- src/main/java/com/quiz/Quiz_App/Controller/QuestionController.java
+++ src/main/java/com/quiz/Quiz_App/Controller/QuestionController.java
@@ -7,7 +7,6 @@ import com.quiz.Quiz_App.Service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
